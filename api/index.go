7f04723c756750b217af0ee8400336b73f0438b7
zach <zachflentgewong@gmail.com>
2024-05-06 19:53:21 -0700

    trying the whole project with embedded files

--- api/index.go
+++ api/index.go
@@ -9,17 +9,17 @@ import (
 	"net/http"
 )
 
-// type StackData struct {
-// 	Path   template.HTMLAttr
-// 	Width  int
-// 	Height int
-// 	Gap    int
-// 	Title  string
-// }
+type StackData struct {
+	Path   template.HTMLAttr
+	Width  int
+	Height int
+	Gap    int
+	Title  string
+}
 
-// type TagData struct {
-// 	Title string
-// }
+type TagData struct {
+	Title string
+}
 
 // var templates = template.Must(template.ParseFiles(
 // 	"home.html",
@@ -33,181 +33,219 @@ import (
 // 	"tag.html",
 // ))
 
-// var techStack = map[string][]StackData{
-// 	"tech": {
-// 		{
-// 			Path:   template.HTMLAttr("src/public/go.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Gap:    2,
-// 			Title:  "Go",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/java.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "Java",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/python.svg"),
-// 			Width:  10,
-// 			Height: 10,
-// 			Gap:    2,
-// 			Title:  "Python",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/django.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "Django",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/postgres.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "PostgreSQL",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/node.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    0,
-// 			Title:  "Node.js",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/next.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "Next.js",
-// 		},
-// 		{
-// 			Path: template.HTMLAttr("src/public/supabase.svg"),
+var techStack = map[string][]StackData{
+	"tech": {
+		{
+			Path:   template.HTMLAttr("src/public/go.svg"),
+			Width:  40,
+			Height: 40,
+			Gap:    2,
+			Title:  "Go",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/java.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "Java",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/python.svg"),
+			Width:  10,
+			Height: 10,
+			Gap:    2,
+			Title:  "Python",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/django.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "Django",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/postgres.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "PostgreSQL",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/node.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    0,
+			Title:  "Node.js",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/next.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "Next.js",
+		},
+		{
+			Path: template.HTMLAttr("src/public/supabase.svg"),
 
-// 			Gap:   1,
-// 			Title: "Supabase",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/swift.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "Swift",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/git.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "Git",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/react.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    2,
-// 			Title:  "React.js",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/tailwind.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Gap:    0,
-// 			Title:  "TailwindCSS",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/html.svg"),
-// 			Width:  30,
-// 			Height: 30,
-// 			Title:  "HTML5",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/htmx.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "HTMX",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/css.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "CSS3",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/javascript.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "Javascript",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/typescript.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "Typescript",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/astro.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "Astro.js",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/bootstrap.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "Bootstrap",
-// 		},
-// 		{
-// 			Path:   template.HTMLAttr("src/public/chakra.svg"),
-// 			Width:  40,
-// 			Height: 40,
-// 			Title:  "ChakraUI",
-// 		},
-// 	},
-// }
+			Gap:   1,
+			Title: "Supabase",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/swift.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "Swift",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/git.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "Git",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/react.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    2,
+			Title:  "React.js",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/tailwind.svg"),
+			Width:  30,
+			Height: 30,
+			Gap:    0,
+			Title:  "TailwindCSS",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/html.svg"),
+			Width:  30,
+			Height: 30,
+			Title:  "HTML5",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/htmx.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "HTMX",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/css.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "CSS3",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/javascript.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "Javascript",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/typescript.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "Typescript",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/astro.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "Astro.js",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/bootstrap.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "Bootstrap",
+		},
+		{
+			Path:   template.HTMLAttr("src/public/chakra.svg"),
+			Width:  40,
+			Height: 40,
+			Title:  "ChakraUI",
+		},
+	},
+}
 
-// var tags = map[string][]TagData{
-// 	"bbyHtmx": {
-// 		{Title: "Astro"},
-// 		{Title: "HTMX"},
-// 		{Title: "Alpine.js"},
-// 		{Title: "Javascript"},
-// 		{Title: "Tailwind"},
-// 		{Title: "OpenAI"},
-// 		{Title: "Supabase"},
-// 	},
-// 	"nxtStore": {
-// 		{Title: "Next.js"},
-// 		{Title: "React.js"},
-// 		{Title: "Zustand"},
-// 		{Title: "Tailwind"},
-// 		{Title: "Axios"},
-// 		{Title: "Shadcn"},
-// 	},
-// 	"gamePlus": {
-// 		{Title: "React.js"},
-// 		{Title: "ChakraUI"},
-// 		{Title: "Vite"},
-// 		{Title: "Axios"},
-// 		{Title: "Zustand"},
-// 	},
-// }
+var tags = map[string][]TagData{
+	"bbyHtmx": {
+		{Title: "Astro"},
+		{Title: "HTMX"},
+		{Title: "Alpine.js"},
+		{Title: "Javascript"},
+		{Title: "Tailwind"},
+		{Title: "OpenAI"},
+		{Title: "Supabase"},
+	},
+	"nxtStore": {
+		{Title: "Next.js"},
+		{Title: "React.js"},
+		{Title: "Zustand"},
+		{Title: "Tailwind"},
+		{Title: "Axios"},
+		{Title: "Shadcn"},
+	},
+	"gamePlus": {
+		{Title: "React.js"},
+		{Title: "ChakraUI"},
+		{Title: "Vite"},
+		{Title: "Axios"},
+		{Title: "Zustand"},
+	},
+}
 
-//go:embed templates/*
+//go:embed src/*
 var templateFiles embed.FS
+var templates = template.Must(template.ParseFS(templateFiles, "src/templates/*.html", "src/templates/components/*.html"))
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	templates.ExecuteTemplate(w, "index.html", techStack)
+// }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	templates := template.Must(template.ParseFS(templateFiles, "templates/*.html"))
-	templates.ExecuteTemplate(w, "links.html", nil)
+	switch r.URL.Path {
+	case "/":
+		// fmt.Fprintf(w, "<h1>Success</h1>")
+		RenderTemplate(w, "index", techStack)
+	case "/projects":
+		if r.Header.Get("HX-Request") == "true" {
+			RenderTemplate(w, "projects", tags)
+		} else {
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	case "/blog":
+		if r.Header.Get("HX-Request") == "true" {
+			RenderTemplate(w, "blog", tags)
+		} else {
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	case "/main":
+		if r.Header.Get("HX-Request") == "true" {
+			RenderTemplate(w, "main-htmx", techStack)
+		} else {
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	default:
+		http.NotFound(w, r)
+	}
 }
 
 func Main() {
 	router := http.NewServeMux()
 
+	fs := http.FileServer(http.Dir("src"))
+	router.Handle("GET /src/", http.StripPrefix("/src/", fs))
+
 	router.HandleFunc("GET /", Handler)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
